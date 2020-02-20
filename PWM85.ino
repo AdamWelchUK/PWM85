dc6d5db855cb3dba4ab8bccdea6f07898cc4b26a
couin3 <35873376+couin3@users.noreply.github.com>
2020-02-20 13:55:51 +0100

    Adding watchdog sleep and other minor changes
    * Adding ATtiny schematic in header comments
    * Disabling USI in PRR -> Around 5µA reduction (3.3V@1MHz)
    * Also adding pinMode setup for all pins -> Correct unwanted LED behavior at startup
    * And making a watchdog sleep (idle) instead of "delay(10)" -> Around 100µA reduction (3.3V@1MHz)

--- PWM85.ino
+++ PWM85.ino
@@ -2,10 +2,10 @@
   but adapted to work on the ATTiny25/45/85 by Adam Welch (http://adamwelch.co.uk).
   In arduino IDE you will need to add the following to your Additional Board Manager URLs within Preferences:
   https://raw.githubusercontent.com/damellis/attiny/ide-1.6.x-boards-manager/package_damellis_attiny_index.json
-  
-  For the highest efficiency you should change the ATTiny to an internal 1MHz clock speed - 
+
+  For the highest efficiency you should change the ATTiny to an internal 1MHz clock speed -
   to apply this setting click Burn Bootloader in the Arduino IDE.
-  
+
   This code should work on all ATTiny25/45/85s but make sure to select the right one when uploading your code.
   To get thre PWM outputs on the ATTiny*5 (and set them reliably) I found these sites super useful:
   https://forum.arduino.cc/index.php?topic=134754.msg1013479#msg1013479
@@ -24,8 +24,25 @@
   R2 = value of R2 in the circuit (20000 ohms by design)
 */
 
+//                ATMEL ATTiny85
+//                     +-\/-+
+//               RST  1|    |8  VCC
+//     LED (D 3) PB3  2|    |7  PB2 (D 2) SCK
+//  DRIVER (D 4) PB4  3|    |6  PB1 (D 1) MISO
+//               GND  4|    |5  PB0 (D 0) MOSI
+//                     +----+
+
+// Fuse Settings : Low=0x62, High=0xDF, Ext=0xFF, Lock= 0xFF (1MHz Clock Bootloader)
+// Seems no software BOD disable is available on my ATtiny version (fairly common)
+
+#include <avr/power.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+
+#define wdt_int() WDTCR |= _BV(WDIE) // | _BV(WDCE) | _BV(WDE) // WDT goes to interrupt, not reset
+
 #define floatVoltage 13.5
-#define regulatorVoltage 5.0
+#define regulatorVoltage 3.3
 #define R1 82000
 #define R2 20000
 
@@ -36,6 +53,15 @@ int pulseWidth = 0;
 int stepSize = 0;
 
 void setup() {
+
+  power_usi_disable(); // Power Register - Shuts down the USI
+  set_sleep_mode(SLEEP_MODE_IDLE); // Configure attiny85 sleep mode
+
+  pinMode(0, OUTPUT); // MOSI - PUMP1
+  pinMode(1, OUTPUT); // MISO - PUMP2
+  pinMode(2, INPUT);  // SCK  - MES
+  pinMode(3, OUTPUT); // LED
+  pinMode(4, OUTPUT); // DRIVER
 
   TCCR0A = 2 << COM0A0 | 3 << COM0B0 | 3 << WGM00; // Timer 0 Control Register A - Set None Inverting Mode in Fast PWM, Set Inverting Mode in Fast PWM, Enable Fast PWM (11.9.2 in ATTiny85 datasheet)
   TCCR0B = 0 << WGM02 | 1 << CS00; // Timer 0 Control Register B - Enable Fast PWM, Clock Select Bit no prescaling (11.9.3 in ATTiny85 datasheet)
@@ -57,15 +83,24 @@ void loop() {
   {
     pulseWidth += stepSize;
     if (pulseWidth > 255) pulseWidth = 255;
-    
+
   }
   else if (measurement > setPoint)
   {
-    pulseWidth -= stepSize;    
+    pulseWidth -= stepSize;
     if (pulseWidth < 0) pulseWidth = 0;
-    
+
   }
   analogWrite(4, pulseWidth); //ATTiny Pin 3
   analogWrite(3, (255 - pulseWidth));
-  delay(10);
+
+  // delay(10);
+  wdt_enable(WDTO_15MS); // prescale of 8 ~= 15msec
+  sleep_mode();          // Make CPU sleep until next WDT interrupt
+  wdt_disable();
+}
+
+// Watchdog Timer Interrupt Service / is executed when watchdog timed out
+ISR(WDT_vect) {
+  wdt_int(); // Needed each time
 }
