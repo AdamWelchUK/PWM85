8bbae583ee8b984646c05ee18d0b2d1786efbc29
AdamWelchUK <38890921+AdamWelchUK@users.noreply.github.com>
2021-04-06 20:07:29 +0100

    Update PWM85.ino
    Rremove need for two files

--- PWM85.ino
+++ PWM85.ino
@@ -6,8 +6,6 @@
 #include <avr/wdt.h>
 #include <avr/sleep.h>
 
-#include "PWM85.h"
-
 /*
   ArduinoIDE
   To program the ATTiny in the arduino IDE you will need the board definitions installing.  I suggest installing
@@ -22,7 +20,42 @@
   To get three PWM outputs on the ATTiny (and set them reliably) I found these sites super useful:
   https://forum.arduino.cc/index.php?topic=134754.msg1013479#msg1013479
   http://www.technoblogy.com/show?LE0
+
+                 ATMEL ATTiny85
+                     +-\/-+
+               RST  1|    |8  VCC
+     LED (D 3) PB3  2|    |7  PB2 (A 1) SCK
+  DRIVER (D 4) PB4  3|    |6  PB1 (D 1) MISO
+               GND  4|    |5  PB0 (D 0) MOSI
+                     +----+
+
+  Fuse Settings : Low=0x62, High=0xDF, Ext=0xFF, Lock= 0xFF (1MHz Clock Bootloader)
+  Seems no software BOD disable is available on my ATtiny version (fairly common)
 */
+
+#define PMP1_Pin 0 //MOSI - PUMP1
+#define PMP2_Pin 1 //MISO - PUMP2
+#define VBAT_Pin 2 //SCK  - VBAT Mes
+#define LED_Pin  3 //LED
+#define DRV_Pin  4 //Driver
+
+/* Battery Charging Set Point.
+
+  This equation is calculated at compile time to set the interger which the charge controller uses as it's modulation set point.
+  By default this is set to a value to charge the battery to 13.5 volts.  Change this number if you wish - absolute maximum 16.8v
+
+  It is also reconmended to test the output of the regulator and use this equation to calibrate the charge controller.
+
+  floatV = battery float voltage (13.5 volts by default)
+  regulatorV = output from the voltage regulator (5.0 or 3.3 volts typically - but check yours!)
+  R1 = value of R1 in the circuit (82000 ohms by design)
+  R2 = value of R2 in the circuit (20000 ohms by design)
+*/
+
+#define floatV 13.5
+#define regulatorV 3.3
+#define R1 82000
+#define R2 20000
 
 #define wdt_int() WDTCR |= _BV(WDIE) // | _BV(WDCE) | _BV(WDE) // WDT goes to interrupt, not reset
 
